9ca18312e04a54ecb3b14a825b947f4b26005b07
Colin King <colin.king.96@gmail.com>
2016-04-03 00:35:42 -0400

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	browserify = require('gulp-browserify'),
+	rename = require('gulp-rename');
 
 // Compile SCSS files into CSS
 gulp.task('sass', function () {
-	return gulp.src('public/sass/**/*.scss')
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(gulp.dest('public/css'));
+  return gulp.src('public/sass/**/*.scss')
+  .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+  .pipe(gulp.dest('public/css'));
 });
 
-// Default task to watch files 
-gulp.task('default', function() {
-    gulp.watch('public/sass/**/*.scss', ['sass']);
+// Basic usage
+gulp.task('scripts', function () {
+  // Single entry point to browserify
+  gulp.src('public/js/main.js')
+		.pipe(browserify({ insertGlobals: true }))
+		.pipe(rename('main-built.js'))
+		.pipe(gulp.dest('public/js'));
+});
+
+// Default task to watch files
+gulp.task('default', function () {
+  gulp.watch('public/sass/**/*.scss', ['sass', 'scripts']);
 });
