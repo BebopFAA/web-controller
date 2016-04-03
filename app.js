41939c7a3eb5b17b7c7214ce9385ce9733939fed
Sean Bae <s@seanbae.net>
2016-04-03 10:47:52 -0400

    demo update

--- app.js
+++ app.js
@@ -73,13 +73,13 @@ var ANGULAR_VEL = 100;
 Cylon.robot({
 	connections: {
 		keyboard: { adaptor: 'keyboard' },
-		// joystick: { adaptor: "joystick" },
+		joystick: { adaptor: "joystick" },
 		bebop: { adaptor: 'bebop' },
 	},
 
 	devices: {
 		keyboard: { driver: 'keyboard', connection: 'keyboard' },
-		// controller: { driver: "dualshock-4", connection: "joystick" },
+		controller: { driver: "dualshock-4", connection: "joystick" },
 		drone: { driver: 'bebop', connection: 'bebop' },
 	},
 
@@ -89,177 +89,226 @@ Cylon.robot({
 		console.log('\t Linear velocity (z):\t' + LINEAR_Z_VEL);
 		console.log('\t Angular velocity:\t' + ANGULAR_VEL);
 
-		console.log(my.drone.connection.connector.GPSSettings.resetHome());
-		console.log(my.drone.connection.connector.WifiSettings.outdoorSetting(1));
+		// my.drone.connection.connector.GPSSettings.resetHome();
+		// my.drone.connection.connector.WifiSettings.outdoorSetting(1);
 
-		my.drone.on('PositionChanged', function(data) {
-			console.log(data);
-		});
+		// console.log(my.drone)
 
-		my.drone.on('battery', function(data) {
-			console.log(data);
-		});
+		// PlayStation Dualshock 4 controller
+    	var that = this,
+		rightStick = { x: 0.0, y: 0.0 },
+		leftStick = { x: 0.0, y: 0.0 };
 
+		// socket io connection
 	    io.on('connection', function (socket) {
-	      console.log('Socket IO connection established!');
+			console.log('Socket IO connection established!');
 
-	      my.drone.on('video', function (data) {
-	        // console.log('Transmitting video data...')
-	        socket.emit('data', data.toString('base64'));
-	      });
+			my.drone.on('video', function (data) {
+				socket.emit('data', data.toString('base64'));
+			});
+
+			my.drone.on('PositionChanged', function(data) {
+				console.log(data);
+				socket.emit('position', data);
+			});
+
+			my.drone.on('battery', function(data) {
+				console.log(data);
+				socket.emit('battery', data);
+			});
+
+			my.drone.on('ready', function() {
+				console.log('drone is ready...');
+				socket.emit('state', 'ready');
+			});
+
+			my.drone.on('flying', function() {
+				console.log('drone is flying...');
+				socket.emit('state', 'flying');
+			});
+
+			my.drone.on('hovering', function() {
+				console.log('drone is hovering...');
+				socket.emit('state', 'hovering');
+			});
+
+			my.drone.on('landed', function() {
+				console.log('drone just landed...');
+				socket.emit('state', 'landed');
+			});
+
+			my.drone.on('landing', function() {
+				console.log('drone is landing...');
+				socket.emit('state', 'landing');
+			});
+
+			my.drone.on('takingOff', function() {
+				console.log('drone is taking off...');
+				socket.emit('state', 'takingOff');
+			});
+
+			that.controller.on("square:press", function() {
+				that.drone.takeOff();
+			});
+
+			that.controller.on("triangle:press", function() {
+				that.drone.stop();
+			});
+
+			that.controller.on("x:press", function() {
+				socket.emit('button', 'x:press');
+				that.drone.land();
+			});
+
+			that.controller.on("right_x:move", function(data) {
+				console.log('right_x:move: ' + data);
+				rightStick.x = data;
+			});
+
+			that.controller.on("right_y:move", function(data) {
+				console.log('right_y:move' + data);
+				rightStick.y = data;
+			});
+
+			that.controller.on("right_stick:release", function(data) {
+				console.log('right_stick:release');
+			});
+
+			that.controller.on("left_x:move", function(data) {
+				console.log('left_x:move' + data);
+				leftStick.x = data;
+			});
+
+			that.controller.on("left_y:move", function(data) {
+				console.log('left_y:move' + data);
+				leftStick.y = data;
+			});
+
+	    	that.controller.on("l2:press", function(data) {
+	    		socket.emit('button', 'l2:press');
+	    		that.drone.up(LINEAR_Z_VEL)
+	    	});
+
+	    	that.controller.on("r2:press", function(data) {
+	    		socket.emit('button', 'r2:press');
+	    		that.drone.down(LINEAR_Z_VEL)
+	    	});
+
+	    	that.controller.on("l2:release", function(data) {
+	    		socket.emit('button', 'l2:release');
+	    		that.drone.stop()
+	    	});
+
+	    	that.controller.on("r2:release", function(data) {
+	    		socket.emit('button', 'r2:release');
+		    	that.drone.stop()
+		    });
 	    });
 
-    // var that = this,
-    //				rightStick = { x: 0.0, y: 0.0 },
-    //				leftStick = { x: 0.0, y: 0.0 };
 
-    //		that.controller.on("square:press", function() {
-    //			that.drone.takeOff();
-    //		});
+		setInterval(function() {
+			var pair = leftStick;
 
-    //		that.controller.on("triangle:press", function() {
-    //			that.drone.stop();
-    //		});
+			if (pair.y < 0) {
+				that.drone.forward(validatePitch(pair.y));
+			} else if (pair.y > 0) {
+				that.drone.backward(validatePitch(pair.y));
+			}
 
-    //		that.controller.on("x:press", function() {
-    //			that.drone.land();
-    //		});
+			if (pair.x > 0) {
+				that.drone.right(validatePitch(pair.x));
+			} else if (pair.x < 0) {
+				that.drone.left(validatePitch(pair.x));
+			}
+		}, 0);
 
-    //		that.controller.on("right_x:move", function(data) {
-    //			rightStick.x = data;
-    //		});
+		setInterval(function() {
+			var pair = rightStick;
 
-    //		that.controller.on("right_y:move", function(data) {
-    //			rightStick.y = data;
-    //		});
+			if (pair.y < 0) {
+				that.drone.up(validatePitch(pair.y));
+			} else if (pair.y > 0) {
+				that.drone.down(validatePitch(pair.y));
+			}
 
-    //		that.controller.on("left_x:move", function(data) {
-    //			leftStick.x = data;
-    //		});
+			if (pair.x > 0) {
+				that.drone.clockwise(validatePitch(pair.x));
+			} else if (pair.x < 0) {
+				that.drone.counterClockwise(validatePitch(pair.x));
+			}
+		}, 0);
 
-    //		that.controller.on("left_y:move", function(data) {
-    //			leftStick.y = data;
-    //		});
+		setInterval(function() {
+			that.drone.stop();
+		}, 10);
 
-    // 	that.controller.on("l2:press", function(data) {
-    // 	that.drone.up(LINEAR_Z_VEL)
-    // });
+		// keyboard controls
 
-    // 	that.controller.on("r2:press", function(data) {
-    // 	that.drone.down(LINEAR_Z_VEL)
-    // });
+		// take off
+		my.keyboard.on('o', function () {
+			console.log('TAKING OFF');
+			my.drone.takeOff();
+		});
 
-    // 		that.controller.on("l2:release", function(data) {
-    // 	that.drone.stop()
-    // });
+		// land
+		my.keyboard.on('p', function () {
+			console.log('LANDING');
+			my.drone.land();
+		});
 
-    // 	that.controller.on("r2:release", function(data) {
-    // 	that.drone.stop()
-    // });
+		// increase altitude
+		my.keyboard.on('up', function () {
+			console.log('MOVING UP');
+			my.drone.up(LINEAR_Z_VEL);
+		});
 
-    //		setInterval(function() {
-    //			var pair = leftStick;
+		// decrease altitude
+		my.keyboard.on('down', function () {
+			console.log('MOVING DOWN');
+			my.drone.down(LINEAR_Z_VEL);
+		});
 
-    //			if (pair.y < 0) {
-    //				that.drone.forward(validatePitch(pair.y));
-    //			} else if (pair.y > 0) {
-    //				that.drone.backward(validatePitch(pair.y));
-    //			}
+		// spin counter-clockwise
+		my.keyboard.on('q', function () {
+			console.log('SPINNING CCW');
+			my.drone.counterClockwise(ANGULAR_VEL);
+		});
 
-    //			if (pair.x > 0) {
-    //				that.drone.right(validatePitch(pair.x));
-    //			} else if (pair.x < 0) {
-    //				that.drone.left(validatePitch(pair.x));
-    //			}
-    //		}, 0);
+		// spin clockwise
+		my.keyboard.on('e', function () {
+			console.log('SPINNING CW');
+			my.drone.clockwise(ANGULAR_VEL);
+		});
 
-    //		setInterval(function() {
-    //			var pair = rightStick;
+		// move forward
+		my.keyboard.on('w', function () {
+			console.log('MOVING FORWARD');
+			my.drone.forward(LINEAR_XY_VEL);
+		});
 
-    //			if (pair.y < 0) {
-    //				that.drone.up(validatePitch(pair.y));
-    //			} else if (pair.y > 0) {
-    //				that.drone.down(validatePitch(pair.y));
-    //			}
+		// move left
+		my.keyboard.on('a', function () {
+			console.log('MOVING LEFT');
+			my.drone.left(LINEAR_XY_VEL);
+		});
 
-    //			if (pair.x > 0) {
-    //				that.drone.clockwise(validatePitch(pair.x));
-    //			} else if (pair.x < 0) {
-    //				that.drone.counterClockwise(validatePitch(pair.x));
-    //			}
-    //		}, 0);
+		// move backward
+		my.keyboard.on('s', function () {
+			console.log('MOVING BACKWARD');
+			my.drone.backward(LINEAR_XY_VEL);
+		});
 
-    //		setInterval(function() {
-    //			that.drone.stop();
-    //		}, 10);
+		// move right
+		my.keyboard.on('d', function () {
+			console.log('MOVING RIGHT');
+			my.drone.right(LINEAR_XY_VEL);
+		});
 
-    // keyboard controls
-
-    // take off
-    my.keyboard.on('o', function () {
-      console.log('TAKING OFF');
-      my.drone.takeOff();
-    });
-
-    // land
-    my.keyboard.on('p', function () {
-      console.log('LANDING');
-      my.drone.land();
-    });
-
-    // increase altitude
-    my.keyboard.on('up', function () {
-      console.log('MOVING UP');
-      my.drone.up(LINEAR_Z_VEL);
-    });
-
-    // decrease altitude
-    my.keyboard.on('down', function () {
-      console.log('MOVING DOWN');
-      my.drone.down(LINEAR_Z_VEL);
-    });
-
-    // spin counter-clockwise
-    my.keyboard.on('q', function () {
-      console.log('SPINNING CCW');
-      my.drone.counterClockwise(ANGULAR_VEL);
-    });
-
-    // spin clockwise
-    my.keyboard.on('e', function () {
-      console.log('SPINNING CW');
-      my.drone.clockwise(ANGULAR_VEL);
-    });
-
-    // move forward
-    my.keyboard.on('w', function () {
-      console.log('MOVING FORWARD');
-      my.drone.forward(LINEAR_XY_VEL);
-    });
-
-    // move left
-    my.keyboard.on('a', function () {
-      console.log('MOVING LEFT');
-      my.drone.left(LINEAR_XY_VEL);
-    });
-
-    // move backward
-    my.keyboard.on('s', function () {
-      console.log('MOVING BACKWARD');
-      my.drone.backward(LINEAR_XY_VEL);
-    });
-
-    // move right
-    my.keyboard.on('d', function () {
-      console.log('MOVING RIGHT');
-      my.drone.right(LINEAR_XY_VEL);
-    });
-
-    // stop the drone when a key is released
-    my.keyboard.on('keyup', function (key) {
-      my.drone.stop();
-    });
-  },
+		// stop the drone when a key is released
+		my.keyboard.on('keyup', function (key) {
+			my.drone.stop();
+		});
+	},
 }).start();
 
 function validatePitch(data) {
